e33f70828dcc3ae17b44220e9a6125d3b974e331
Daniel Hwang <danielleehwang@gmail.com>
2014-09-12 22:57:32 -0700

    fix on non-window

--- .mjolnir/init.lua
+++ .mjolnir/init.lua
@@ -1,28 +1,27 @@
 local hotkey = require "mjolnir.hotkey"
 local grid = require "mjolnir.sd.grid"
+local window = require "mjolnir.window"
 
 grid.GRIDWIDTH = 2
 grid.GRIDHEIGHT = 1
 
-local function pushwindow_left()
-  grid.adjust_focused_window(function(f)
-    f.x = 0
-    f.y = 0
-    f.w = grid.GRIDWIDTH/2
-    f.h = grid.GRIDHEIGHT+1
-  end)
-end
+local function pushwindow(d)
+  if window.focusedwindow() ~= nil then
+    local x = 0
+    if d == 'left' then x = 0
+    elseif d == 'right' then x = grid.GRIDWIDTH-1
+    end
 
-local function pushwindow_right()
-  grid.adjust_focused_window(function(f)
-    f.x = grid.GRIDWIDTH-1
-    f.y = 0
-    f.w = grid.GRIDWIDTH/2
-    f.h = grid.GRIDHEIGHT+1
-  end)
+    grid.adjust_focused_window(function(f)
+      f.x = x
+      f.y = 0
+      f.w = grid.GRIDWIDTH/2
+      f.h = grid.GRIDHEIGHT+1
+    end)
+  end
 end
 
 mash = {"cmd", "ctrl"}
-hotkey.bind(mash, 'H', pushwindow_left)
-hotkey.bind(mash, 'L', pushwindow_right)
+hotkey.bind(mash, 'H', function() pushwindow('left') end)
+hotkey.bind(mash, 'L', function() pushwindow('right') end)
 

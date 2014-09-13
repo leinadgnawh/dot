05461f568d805a71cf80e1a2a37ea8f6bbc071b2
Daniel Hwang <danielleehwang@gmail.com>
2014-09-12 23:25:21 -0700

    add minimize and maximize

--- .mjolnir/init.lua
+++ .mjolnir/init.lua
@@ -5,7 +5,7 @@ local window = require "mjolnir.window"
 grid.GRIDWIDTH = 2
 grid.GRIDHEIGHT = 1
 
-local function pushwindow(d)
+local function push_window(d)
   if window.focusedwindow() ~= nil then
     local x = 0
     if d == 'left' then x = 0
@@ -21,7 +21,18 @@ local function pushwindow(d)
   end
 end
 
+local function size_window(d)
+  local win = window.focusedwindow()
+  if win ~= nil then
+    if d == 'min' then win:minimize()
+    elseif d == 'max' then grid.maximize_window()
+    end
+  end
+end
+
 mash = {"cmd", "ctrl"}
-hotkey.bind(mash, 'H', function() pushwindow('left') end)
-hotkey.bind(mash, 'L', function() pushwindow('right') end)
+hotkey.bind(mash, 'H', function() push_window('left') end)
+hotkey.bind(mash, 'J', function() size_window('min') end)
+hotkey.bind(mash, 'K', function() size_window('max') end)
+hotkey.bind(mash, 'L', function() push_window('right') end)
 

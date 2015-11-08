ace97a3a1b4062ddf7c0c039246623628d7c5484
Daniel Hwang <gnawhleinad@github.com>
2015-11-08 11:04:36 -0800

    add up/down screen moves

--- .mjolnir/init.lua
+++ .mjolnir/init.lua
@@ -3,20 +3,34 @@ local grid = require "mjolnir.sd.grid"
 local window = require "mjolnir.window"
 
 grid.GRIDWIDTH = 2
-grid.GRIDHEIGHT = 1
+grid.GRIDHEIGHT = 2
 
 local function push_window(d)
   if window.focusedwindow() ~= nil then
     local x = 0
-    if d == 'left' then x = 0
-    elseif d == 'right' then x = grid.GRIDWIDTH-1
+    local y = 0
+    local w = grid.GRIDWIDTH/2
+    local h = grid.GRIDHEIGHT
+
+    if d == 'left' then
+      x = 0
+    elseif d == 'down' then
+      y = grid.GRIDHEIGHT/2
+      w = grid.GRIDWIDTH
+      h = grid.GRIDHEIGHT/2
+    elseif d == 'up' then
+      y = 0
+      w = grid.GRIDWIDTH
+      h = grid.GRIDHEIGHT/2
+    elseif d == 'right' then
+      x = grid.GRIDWIDTH-1
     end
 
     grid.adjust_focused_window(function(f)
       f.x = x
-      f.y = 0
-      f.w = grid.GRIDWIDTH/2
-      f.h = grid.GRIDHEIGHT+1
+      f.y = y
+      f.w = w
+      f.h = h
     end)
   end
 end
@@ -33,9 +47,14 @@ end
 mash = {"cmd", "ctrl"}
 
 hotkey.bind(mash, 'H', function() push_window('left') end)
-hotkey.bind(mash, 'J', function() size_window('min') end)
-hotkey.bind(mash, 'K', function() size_window('max') end)
+hotkey.bind(mash, 'J', function() push_window('down') end)
+hotkey.bind(mash, 'K', function() push_window('up') end)
 hotkey.bind(mash, 'L', function() push_window('right') end)
 
 hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
 hotkey.bind(mash, 'P', grid.pushwindow_nextscreen)
+
+mash = {"cmd", "ctrl", "shift"}
+
+hotkey.bind(mash, 'J', function() size_window('min') end)
+hotkey.bind(mash, 'K', function() size_window('max') end)
